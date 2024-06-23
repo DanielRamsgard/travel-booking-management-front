d80ef9a5c1b632127f0770b25f3eb0d3e24df308
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-06-23 15:03:39 -0400

    w

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Nav from "./components/Nav";
 import Content from "./components/Content";
+import Video from "./components/Video";
 
 function App() {
     return (
         <>
+            <Video />
             <Nav />
             <Content />
-            <video src="" />
         </>
     );
 }
---
+++ src/components/Video.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Video = () => {
+    return (
+        <>
+            <video className="video-slide" src="/static/media/waves.mp4" autoPlay muted loop></video>
+        </>
+    );
+}
+
+export default Video;
