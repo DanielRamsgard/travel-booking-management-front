27d65f94c6cc7ebbc29215e4b3ff7d5a1a74654a
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-06-23 20:56:23 -0400

    work

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -8,6 +8,17 @@ const Flight = () => {
             <div className="airport-container">
                 <img alt="Airport" className="img-container" src="/static/media/airport.jpeg"></img>
             </div>
+            <div className="flight-content-container">
+                <div className="flight-title">
+                    Search Cheap Flight Tickets
+                </div>
+                <div className="flight-subtitle">
+                    Helping you save on flights since 2007
+                </div>
+                <div className="flight-action">
+
+                </div>
+            </div>
         </>
     );
 }
