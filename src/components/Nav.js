aa802105f7078d4d7ff5b7f3ed0f1528d2d71914
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-06-26 16:48:55 -0400

    work

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,10 @@
 import React, { useState }  from "react";
 import useWindowWidth from "./useWindowWidth";
 import NavPage from "./NavPage";
+import { useNavigate } from "react-router-dom";
 
 const Nav = () => {
+    const navigate = useNavigate();
     const widthBool = useWindowWidth(700);
     const [nav, setNav] = useState(false);
 
@@ -15,7 +17,7 @@ const Nav = () => {
             { nav ? <NavPage updateNav={updateNav}/> : <></> }
             <div className="nav-container">
                 <div className="nav-container-2">
-                    <a href="/" className="travel"> TRAVEL </a>
+                    <div onClick={() => {navigate("/")}} className="travel"> TRAVEL </div>
                     { widthBool ?
                     <div className="nav-container-3" onClick={() => {updateNav(true);}}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" viewBox="0 0 16 16">
@@ -24,11 +26,11 @@ const Nav = () => {
                     </div>
                     : 
                     <div className="nav-container-3">
-                        <a href="/" className="nav-text"> Home <div className="bar"></div> </a>
-                        <a href="/flight" className="nav-text"> Flights <div className="bar"></div> </a>
-                        <a href="/hotel" className="nav-text"> Hotels <div className="bar"></div> </a>
-                        <a href="/about" className="nav-text"> About <div className="bar"></div> </a>
-                        <a href="/contact" className="nav-text"> Contact <div className="bar"></div> </a>
+                        <div onClick={() => {navigate("/")}} className="nav-text"> Home <div className="bar"></div> </div>
+                        <div onClick={() => {navigate("/flight")}} className="nav-text"> Flights <div className="bar"></div> </div>
+                        <div onClick={() => {navigate("/hotel")}} className="nav-text"> Hotels <div className="bar"></div> </div>
+                        <div onClick={() => {navigate("/about")}} className="nav-text"> About <div className="bar"></div> </div>
+                        <div onClick={() => {navigate("/contact")}} className="nav-text"> Contact <div className="bar"></div> </div>
                     </div>}
                     
                 </div>
